cd9edaa9396fe2360f5b15d6f15875125f51509b
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2025-01-23 18:33:31 +0100

    Add html framing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,129 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="flex flex-col min-h-screen ">
+        <header className="bg-white h-[53px]">
+          <div className="flex justify-between px-10 items-center h-full">
+            {/* Logo and Search */}
+            <div className="flex space-x-5">
+              <div>Logo</div>
+              <div>Search</div>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            {/* Tabs, Profile and Business */}
+            <div className="flex space-x-10">
+              <div className="flex space-x-5">
+                <div>Home</div>
+                <div>My Network</div>
+                <div>Jobs</div>
+                <div>Messaging</div>
+                <div>Notifications</div>
+                <div>Me</div>
+              </div>
+
+              <div className="flex space-x-5">
+                <div>For Business</div>
+                <div>Advertise</div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main className="bg-[#F4F2EE] grow border-2 ">
+          <div className="flex px-10 gap-5">
+            {/* Main Content */}
+            <div className="w-3/4">
+              {/* Profile Section */}
+              <section className=" rounded-xl bg-white">
+                {/* Cover photo */}
+                <div>
+                  <Image
+                    src="/cover-photo.jfif"
+                    alt="Cover photo"
+                    layout="responsive"
+                    width={1400}
+                    height={350}
+                  ></Image>
+                </div>
+
+                {/* Details */}
+                <div className="p-4">
+                  <div className="flex flex-col gap-2">
+                    <div>
+                      <h1 className="text-2xl font-bold">Steffen Grøn Andersen</h1>
+                      <p>Full Stack Developer</p>
+                    </div>
+                    <div>
+                      <p className="text-sm text-[#666666] font-medium">
+                        Copenhagen, Capital Region of Denmark, Denmark
+                        <span className="ml-1 text-blue-500 font-bold">Contact info</span>
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-sm text-[#666666] font-medium">500+ connections</p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+              {/* About */}
+              <section>About</section>
+
+              {/* Featured */}
+              <section>Featured</section>
+
+              {/* Activity */}
+              <section>Activity</section>
+
+              {/* Experience */}
+              <section>Experience</section>
+
+              {/* Education */}
+              <section>Education</section>
+
+              {/* Licenses & certifications */}
+              <section>Licenses & certifications</section>
+
+              {/* Volunteering */}
+              <section>Volunteering</section>
+
+              {/* Skills */}
+              <section>Skills</section>
+
+              {/* Recommendations */}
+              <section>Recommendations</section>
+
+              {/* Languages */}
+              <section>Languages</section>
+
+              {/* Interests */}
+              <section>Interests</section>
+            </div>
+
+            {/* Right Sidebar */}
+            <div className="w-1/4 border ">
+              {/* Promoted */}
+              <section>Promoted</section>
+
+              {/* More profiles like you */}
+              <section>More profiles like you</section>
+
+              {/* Exlore Premium profiles */}
+              <section>Explore Premium profiles</section>
+
+              {/* People you may know */}
+              <section>People you may know</section>
+
+              {/* You might like */}
+              <section>You might like</section>
+
+              {/* Promoted */}
+              <section>Promoted</section>
+            </div>
+          </div>
+        </main>
+        <footer className="h-[190px] bg-gray-100">Footer</footer>
+      </div>
+    </>
   );
 }
