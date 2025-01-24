a02a7039ccc9d7b3903c375bf5980b7a7b902964
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2025-01-24 23:19:53 +0100

    Add final touches to experience section

--- src/app/sections/experience-section.tsx
+++ src/app/sections/experience-section.tsx
@@ -1,76 +1,12 @@
 import Image from "next/image";
+import jobs from "./experience.json";
 
 export default function ExperienceSection() {
-  const jobs = [
-    {
-      id: 1,
-      image: "/logo/confectio_logo.jfif",
-      title: "Junior Frontend Developer and Marketing Specialist",
-      name: "Confect ApS",
-      start: "Mar 2024",
-      end: "Present",
-      place: "Copenhagen, Capital Region, Denmark",
-      description: "Development on Confect.io in Next.js and supporting marketing.",
-    },
-    {
-      id: 2,
-      image: "/logo/estaid_logo.jfif",
-      title: "Junior Developer",
-      name: "Estaid",
-      start: "Sep 2022",
-      end: "Feb 2024",
-      place: "Copenhagen, Capital Region, Denmark",
-      description:
-        "Development of basic functionality to our service in Vue.js and supporting sales and marketing.",
-    },
-    {
-      id: 3,
-      image: "/logo/kontainer_logo.jfif",
-      title: "Head of Growth and Marketing",
-      name: "Kontainer - Digital Asset Management since 2001",
-      start: "Feb 2022",
-      end: "Aug 2022",
-      place: "Copenhagen, Capital Region, Denmark",
-      description: "Responsible for CRM implementation and marketing activities.",
-    },
-    {
-      id: 4,
-      image: "/logo/beyond11_logo.jfif",
-      title: "Paid Social Specialist",
-      name: "SPASIBA",
-      start: "Oct 2021",
-      end: "Jan 2022",
-      place: "Copenhagen, Capital Region, Denmark",
-      description: "Responsible for developing and executing campaigns with our clients.",
-    },
-    {
-      id: 5,
-      image: "/logo/lead_supply_aps_logo.jfif",
-      title: "Head of Paid Traffic",
-      name: "Karman Connect A/S",
-      start: "Dec 2022",
-      end: "Sep 2021",
-      place: "Copenhagen, Capital Region, Denmark",
-      description: "Responsible for and executing on all marketing channels.",
-    },
-    {
-      id: 6,
-      image: "/logo/web2media_aps_logo.jfif",
-      title: "Digital Marketing Specialist",
-      name: "Web2Media ApS",
-      start: "Jan 2014",
-      end: "Nov 2022",
-      place: "Copenhagen, Capital Region, Denmark",
-      description:
-        "Strategy and project management, campaign and budget planning, training and mentoring colleagues.",
-    },
-  ];
-
   return (
-    <section className="bg-white rounded-xl p-4 border">
+    <section className="bg-white rounded-xl py-6 px-5 border">
       <h2 className="text-xl font-bold mb-5">Experience</h2>
 
-      {jobs.map((job) => (
+      {jobs.map((job, index) => (
         <div key={job.id}>
           <div className="flex gap-3">
             <div className="">
@@ -85,7 +21,7 @@ export default function ExperienceSection() {
               <p className="text-sm mt-2">{job.description}</p>
             </div>
           </div>
-          <hr className="border-t border-gray-200 my-4" />
+          {index < jobs.length - 1 && <hr className="border-t border-gray-200 my-4" />}
         </div>
       ))}
     </section>
